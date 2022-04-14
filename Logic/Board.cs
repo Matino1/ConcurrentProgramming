277add2a97c89cde1ef23ecbb0deec255f766722
236665 Strzelecki Mateusz <236665@edu.p.lodz.pl>
2022-04-14 17:26:57 +0200

    logic + view

---
+++ Logic/Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    internal class Board
+    {
+        public int Size { get; private set; }
+        public List<Ball> Balls { get; private set; }
+
+        public Board(int size, int BallsAmount)
+        {
+            Size = size;
+
+
+            Balls = new List<Ball>();
+            for(int i = 0; i < BallsAmount; i++)
+            {
+                Balls.Add(new Ball());
+            }
+        }
+
+        public void MoveBalls()
+        {
+            while(true)
+            {
+                foreach(Ball ball in Balls)
+                {
+                    ball.ChangeBallPosition(Size);
+                }
+            }
+        }
+
+    }
+}
---
+++ Logic/LogicTest/BallTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTest
+{
+    [TestClass]
+    public class BallTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
